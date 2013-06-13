5a91cfa2a682d022f477949e02536c3bbfc4cfaa
root <root@magali-XPS-M1530.(none)>
2013-06-13 19:53:57 -0300

    Servicios, habitaciones, index

--- js/sendEmail.php
+++ js/sendEmail.php
@@ -19,17 +19,7 @@
 	
 	$headers = "Reply-To: " . $_POST["email"] . "\r\n";
 
-	$cabecera ="Date: ".date("l j F Y, G:i")."\n";
-	$cabecera .="MIME-Version: 1.0\n";
-	$cabecera .="From: ".$nombre."\n";
-	$cabecera .="Return-path: ". $email."\n";
-	$cabecera .="Reply-To: ".$mailTo."\n";
-	$cabecera .="X-Mailer: PHP/". phpversion()."\n";
-
-
 	//mando el correo...
-		
-	// ini_set ("sendmail_from","contacto@aplsconsultoria.com.ar");
 	
 	mail("elhosteldechascomus@gmail.com",$titulo,$cuerpo, $cabecera);
 
